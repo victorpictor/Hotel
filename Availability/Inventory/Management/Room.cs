94f5bbb581fc2df6c2ab0a26eca536973716b5de
victor <vrusu@fareportal.com>
2014-08-11 18:14:14 -0400

    more availability specs

--- Availability/Inventory/Management/Room.cs
+++ Availability/Inventory/Management/Room.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Inventory.Management
 {
@@ -13,7 +14,9 @@ namespace Inventory.Management
 
         public bool IsAvailable(DateTime from, DateTime to)
         {
-            return false;
+            return BookedOn.All(
+                b => (b.CheckIn.Date > from && b.CheckOut.Date >= from) ||
+                     (b.CheckIn.Date < to && b.CheckOut.Date <= to));
         }
     }
 }
--- Spec/Availability.Spec/GetHotelAvailability/OnDateRanges/AndThereAreRoomsAvailable.cs
+++ Spec/Availability.Spec/GetHotelAvailability/OnDateRanges/AndThereAreRoomsAvailable.cs
@@ -1,24 +1,110 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Inventory.Holds;
+using Inventory.Management;
+using Inventory.Services;
+using Inventory.Services.Repositories;
+using Moq;
+using NUnit.Framework;
 
 namespace Availability.Spec.GetHotelAvailability.OnDateRanges
 {
     [TestFixture]
     public class AndThereAreRoomsAvailable: Specification
     {
+
+        private InventoryService inventoryService;
+
+        private Mock<IBookRoomRequestsRepository> requests;
+        private Mock<IRoomRepository> rooms;
+
+        private List<int> searchResult;
+
+        protected override void Given()
+        {
+            requests = new Mock<IBookRoomRequestsRepository>();
+            rooms = new Mock<IRoomRepository>();
+
+            requests.Setup(r => r.Get())
+                    .Returns(new List<SubmittedBookRoomRequest>()
+                        {
+                            new SubmittedBookRoomRequest()
+                                {
+                                    RoomId = 2,
+                                    StartDate = DateTime.Now.AddDays(2).Date,
+                                    EndDate = DateTime.Now.AddDays(8).Date
+                                }
+                        });
+
+            rooms.Setup(r => r.Get(RoomType.All))
+                 .Returns(new List<Room>()
+                     {
+                         new Room()
+                             {
+                                 Id = 1,
+                                 BookedOn =
+                                     new List<BookedOnRange>()
+                                         {
+                                             new BookedOnRange()
+                                                 {
+                                                     CheckIn = DateTime.Now.AddDays(16),
+                                                     CheckOut = DateTime.Now.AddDays(17)
+                                                 }
+                                         }
+                             },
+                          new Room()
+                             {
+                                 Id = 2,
+                                 BookedOn =
+                                     new List<BookedOnRange>()
+                                         {
+                                             new BookedOnRange()
+                                                 {
+                                                     CheckIn = DateTime.Now.AddDays(12).Date,
+                                                     CheckOut = DateTime.Now.AddDays(18).Date
+                                                 }
+                                         }
+                             },
+                             new Room()
+                             {
+                                 Id = 2,
+                                 BookedOn =
+                                     new List<BookedOnRange>()
+                                         {
+                                             new BookedOnRange()
+                                                 {
+                                                     CheckIn = DateTime.Now.AddDays(1).Date,
+                                                     CheckOut = DateTime.Now.AddDays(7).Date
+                                                 }
+                                         }
+                             }
+                     });
+
+            inventoryService = new InventoryService(requests.Object, rooms.Object);
+        }
+
+        protected override void When()
+        {
+            searchResult = inventoryService.AllRooms(DateTime.Now.AddDays(3), DateTime.Now.AddDays(6));
+        }
+
+        [Test]
         public void It_should_get_rooms_by_type()
         {
+            rooms.Verify(r => r.Get(RoomType.All));
         }
 
+        [Test]
         public void It_should_exculude_unavaialble_rooms_for_given_date_range()
         {
+            Assert.AreEqual(1,searchResult.Count,"Should be available one room");
         }
 
+        [Test]
         public void It_should_exculude_avaialble_rooms_for_given_date_range_but_with_a_hold_on_date_range()
         {
-        }
-
-        public void It_should_return_1_room_available()
-        {
+            Assert.AreEqual(1, searchResult.FirstOrDefault(), "Should be available room #1");
         }
 
     }
