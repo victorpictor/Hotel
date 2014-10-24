0983fc846fb88d288b0a3d646ffe22e7fc574815
victor <vrusu@fareportal.com>
2014-10-24 11:13:13 -0400

    submited requests store

---
+++ Infrastructure/InventoryDataAccess/Repositories/SubmittedBookRoomRequests.cs
@@ -0,0 +1,17 @@
+﻿using Inventory.HotelRoom;
+using Inventory.Writes;
+
+namespace InventoryDataAccess.Repositories
+{
+    public class SubmittedBookRoomRequests : IBookRoomRequests
+    {
+        public void Add(SubmittedBookRoomRequest request)
+        {
+            var db = new MongoDbFactory().CreateDb(new Configuration().MongoDbName);
+
+            var requests = db.GetCollection<SubmittedBookRoomRequest>("BookRoomRequests");
+
+            requests.Save(request);
+        }
+    }
+}
